eebd23e16698585f9f39785d6ddcec1a78212360
Zaray <cor191025@uvg.edu.gt>
2021-07-26 17:59:00 -0600

    modelo obj

--- gl.py
+++ gl.py
@@ -1,18 +1,22 @@
 # Interpreta bytes y los empaca como datos binarios
 import struct
 from collections import namedtuple
+import obj
 
 # Creación de un tipo de variable para dibujar una línea
 V2 = namedtuple('Point2', ['x', 'y'])
+
 
 # Permite asegurar que únicamente se utilize un byte
 def char(c):
     # 1 byte, ya que el char de Python ocupa 4 bytes
     return struct.pack('=c', c.encode('ascii'))
 
+
 def word(w):
     # 2 bytes
     return struct.pack('=h', w)
+
 
 def dword(d):
     # 4 bytes
@@ -64,7 +68,7 @@ class Render(object):
         self.viewport(self.width, self.height, 0, 0)
 
     # Viewport
-    def viewport(self, width, height, x, y, color = None):
+    def viewport(self, width, height, x, y, color=None):
         self.vw_width = int(width - 1)
         self.vw_height = int(height - 1)
         self.vw_x = int(x)
@@ -79,12 +83,12 @@ class Render(object):
         self.draw_color = color(r, g, b)
 
     # Dibujar un punto con coordenadas normalizadas
-    def drawPoint_NDC(self, x, y, color = None):
+    def drawPoint_NDC(self, x, y, color=None):
         relative_x = self.vw_x + ((self.vw_width / 2) * (x + 1))
         relative_y = self.vw_y + ((self.vw_height / 2) * (y + 1))
         self.pixels[int(relative_x)][int(relative_y)] = color or self.draw_color
 
-    def drawPoint(self, x, y, color = None):
+    def drawPoint(self, x, y, color=None):
         if x < self.vw_x or x > self.vw_x + self.vw_width or y < self.vw_y or y > self.vw_y + self.vw_height:
             return
 
@@ -93,11 +97,15 @@ class Render(object):
 
     # Bresenham's line algorithm
     # @arg: v0 → vértice inicial, v1 → vértice final
-    def drawLine(self, v0, v1, color = None):
+    def drawLine(self, v0, v1, color=None):
         x0 = v0.x
         x1 = v1.x
         y0 = v0.y
         y1 = v1.y
+
+        if x0 == x1 and y0 == y1:
+            self.drawPoint(x0, y1, color)
+            return
 
         dx = abs(x1 - x0)
         dy = abs(y1 - y0)
@@ -122,7 +130,7 @@ class Render(object):
         # Esto define cuál es el límite para cambiar de pixel
         limit = 0.5
         # Cálculo de la pendiente
-        m = abs(y1 - y0)/abs(x1 - x0)
+        m = abs(y1 - y0) / abs(x1 - x0)
         y = y0
         for x in range(int(x0), int(x1 + 1)):
             if steep:
@@ -132,8 +140,30 @@ class Render(object):
             # Cada cambio en x aumenta la pendiente a offset
             offset += m
             if offset >= limit:
-                y += 1  if y0 < y1 else -1 # Indica el cambio de pixel (decisión)
+                y += 1 if y0 < y1 else -1  # Indica el cambio de pixel (decisión)
                 limit += 1
+
+    def loadModel(self, filename, scale=V2(1, 1), translate=V2(0.0, 0.0)):
+
+        model = obj.obj(filename)
+
+        # draw Model
+        for face in model.faces:
+            vertex_count = len(face)  # Guarda la cantidad de vertices en la cara
+            for x in range(vertex_count):
+                # Se resta 0 porque OBJ empieza en 1 pero list empieza en 0
+                index0 = face[x][0] - 1  # Obtiene el vértice de cada x
+                index1 = face[(x + 1) % vertex_count][0] - 1  # Obtiene el índice del segundo vértice
+                vertex0 = model.vertices[index0]  # Obtiene el vértice a dibujar con respecto a la cara
+                vertex1 = model.vertices[index1]  # Obtiene el vértice a dibujar con respecto a la cara
+
+                x0 = vertex0[0] * scale.x + translate.x
+                y0 = vertex0[1] * scale.x + translate.x
+                x1 = vertex1[0] * scale.x + translate.x
+                y1 = vertex1[1] * scale.x + translate.x
+
+                self.drawLine(V2(x0, y0), V2(x1, y1))
+
 
     '''
     Creación de bitmap
---
+++ obj.py
@@ -0,0 +1,32 @@
+# Cargar archivo OBJ
+
+class obj(object):
+    def __init__(self, filename):
+        with open(filename, 'r') as file:  # Por default open ya está en read-text
+            self.lines = file.read().splitlines()  # Lee el documento línea por línea
+
+        # Variables para almacenar la lectura del documento
+        self.vertices = []
+        self.textures = []  # Coordenadas de texturas
+        self.normals = []
+        self.faces = []
+
+        self.saveData()
+
+
+    def saveData(self):
+        # Formato de OBJ
+        # v -0.8523 -0.6325 -0.5238 → letra = prefijo, valores = coordenadas
+        for line in self.lines:
+            # Asegurar que la línea no está en blanco
+            if line:
+                prefix, value = line.split(' ', 1)  # Separar el prefijo del valor
+                if prefix == 'v':  # Vertices
+                    self.vertices.append(list(map(float, value.split(' '))))
+                elif prefix == 'vt':  # Coordenadas de textura
+                    self.textures.append(value)
+                elif prefix == 'vn':  # Normales
+                    self.normals.append(list(map(float, value.split(' '))))
+                elif prefix == 'f':
+                    self.faces.append([list(map(int, vertex.split('/'))) for vertex in value.split(' ')])
+
