658df51a1894c66ab75cd1246edac6cd05f0d69a
Zaray <cor191025@uvg.edu.gt>
2021-07-26 18:01:20 -0600

    3d ball

--- Engine3D.py
+++ Engine3D.py
@@ -9,4 +9,4 @@ height = 700
 render = gl.Render(width, height)
 render.loadModel('Pallone/Ball OBJ.obj',  gl.V2(320, 320), gl.V2(width/3, height/2))
 
-render.end('output2.bmp')
+render.end('output.bmp')
