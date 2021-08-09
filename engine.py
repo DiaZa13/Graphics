85001f07eee1175e1bbc8a7e718505462ce326cb
Zaray <cor191025@uvg.edu.gt>
2021-08-09 00:48:54 -0600

    lightning

--- engine.py
+++ engine.py
@@ -1,5 +1,5 @@
 # Main
-from libs.gl import Render, V2
+from libs.gl import Render, V2, V3
 
 # Variables
 width = 940
@@ -8,5 +8,5 @@ height = 700
 render = Render(width, height)
 # render.drawTriangle(V2(180, 50), V2(150, 1), V2(70, 180))
 # render.drawTriangle_bc(V2(10, 10), V2(190, 10), V2(100, 190))
-render.loadModel('models/ball.obj', V2(320, 320), V2(width/2, height/2))
+render.loadModel('models/face.obj', V3(300, 300, 300), V3(width/2, height/2, 0))
 render.end('output.bmp')
