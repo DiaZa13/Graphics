1a028170bd6ac44f90dfa27ca4c193d8c8ed5389
Zaray <cor191025@uvg.edu.gt>
2021-08-16 21:45:06 -0600

    SR5: transformations

--- engine.py
+++ engine.py
@@ -3,14 +3,29 @@ from libs.gl import Render, V3
 from libs.obj import Texture
 
 # Variables
-width = 960
-height = 540
+width = 940
+height = 800
 
 render = Render(width, height)
-# render.CreateViewMatrix(V3(-1, 0, 0), V3(0, 30, 0))
-modelPosition = V3(0, 0, -10)
-render.lookAt(modelPosition, V3(-5, 5, 0))
 model_texture = Texture('textures/face.bmp')
-render.loadModel('models/face.obj', model_texture, V3(2, 2, 2), modelPosition, V3(0, 0, 0))
-# render.loadModel('models/face.obj', model_texture, V3(3, 3, 3), V3(-3, 0, -5))
-render.end('output.bmp')
+# -------Low angle
+# modelPosition = V3(0, 5, -3)
+# render.lookAt(modelPosition, V3(0, 6, 3))
+# render.loadModel('models/face.obj', model_texture, V3(3, 3, 3),  modelPosition, V3(-35, 0, 0))
+
+# High angle
+# modelPosition = V3(0, 5, -3)
+# render.lookAt(modelPosition, V3(0, 7, 5))
+# render.loadModel('models/face.obj', model_texture, V3(3, 3, 3),  modelPosition, V3(20, 0, 0))
+
+# Medium shot
+# modelPosition = V3(0, 0, -3)
+# render.lookAt(modelPosition, V3(0, 0, 0))
+# render.loadModel('models/face.obj', model_texture, V3(2, 2, 2),  modelPosition, V3(0, -8, 0))
+
+# Dutch angle
+modelPosition = V3(2, 0, -5)
+render.CreateViewMatrix(V3(0, 0, 0), V3(0, -20, 22))
+render.loadModel('models/face.obj', model_texture, V3(2, 2, 2),  modelPosition, V3(0, 0, 0))
+
+render.end('dutch_angle.bmp')
